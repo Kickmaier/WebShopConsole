bcc65b2cf4e81deba2dd24dd23029e2947bc977d
Tobias Kickmaier <t.kickmaier@hotmail.com>
2026-01-16 10:59:23 +0100

    first

---
+++ Models/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Models
+{
+    internal class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=WebShopInlämning;Trusted_Connection=True; TrustServerCertificate=True;");
+        }
+        public DbSet<Product> Products {get; set;}
+        public DbSet<Category> Categories {get; set;}
+    }
+}
